c67df2d6005141648a36bdf14c555963ac26ac04
ottonsam <ottonsam@gmail.com>
2021-05-26 02:29:52 -0300

    Resolve challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,54 @@
-export function SideBar() {
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  replayGenreID: (number: number) => void;
+  selectedGenreId: number;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  
+
+  function handleClickButton(id: number) {
+    props.replayGenreID(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
